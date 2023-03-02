d3d6d0be56c386204e179ff12980991498f238fd
innsh <innsh@eur.ad.sag@SAG-8V582X2-U.EUR.AD.SAG>
2023-03-02 14:33:00 +0530

    Mysql with multiple databases

---
+++ dockerfile
@@ -0,0 +1,3 @@
+# FROM iregistry.eur.ad.sag/tnvm/mysql:5.7-azid-nocase
+FROM mysql:5.7
+# COPY ./custom.cnf /etc/mysql/conf.d/
---
+++ init/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `pa`;
+GRANT ALL ON `pa`.* TO 'b2b'@'%';
+create database if not exists `fapoolassoc`
+grant all on `fapoolassoc`.* to 'b2b@%';
